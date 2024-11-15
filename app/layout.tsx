820fe8ea3b98a794dd47b0ee11ee4e5cef4cef75
Ronak <ronak01.raj@gmail.com>
2024-11-15 14:07:27 +0545

    web view

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         < Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
