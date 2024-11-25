76606b34488b09caf921edcb9edfd8a330f90021
kritan.thapa <kritan.thapa@swifttech.com.np>
2024-11-25 16:00:09 +0545

    onboarding initial completed

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,34 +1,26 @@
 'use client';
 
-import { useState } from 'react';
-import Image from 'next/image';
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // for carousel
 
 export default function Onboarding() {
   const steps = [
     {
+      image: 'onboarding1.png',
       titleText: 'Your Trusted Identity Powered by Blockchain',
       subtitleText: 'Experience secure, decentralized caller identification with blockchain technology',
     },
     {
+      image: 'onboarding1.png',
       titleText: 'Get rewards for identifying spam',
       subtitleText: 'Help the community by identifying spam callers and messages.',
     },
     {
-      titleText: 'Get rewards for identifying spam',
+      image: 'onboarding1.png',
+      titleText: 'Get rewards for identifying spam (Need to change)',
       subtitleText: 'Help the community by identifying spam callers and messages.',
     },
   ];
-  // const [step, setStep] = useState(0);
-
-  // function incrementStep() {
-  //   setStep(step >= steps.length - 1 ? steps.length - 1 : step + 1);
-  // }
-
-  // function decrementStep() {
-  //   setStep(step <= 0 ? 0 : step - 1);
-  // }
 
   return (
     <div className="flex flex-col items-center justify-start px-8 py-16 h-[100vh] bg-black text-white">
@@ -39,12 +31,12 @@ export default function Onboarding() {
         showIndicators={true}
         showStatus={false}
         renderIndicator={(_, isSelected) => {
-          return <div className={`h-1.5 w-full ${isSelected ? 'bg-violet-600' : 'bg-violet-300'}`}></div>;
+          return <div className={`h-1.5 w-full bg-[#9747FF] rounded ${isSelected ? 'opacity-100' : 'opacity-20'}`}></div>;
         }}
       >
-        {steps.map(({ titleText, subtitleText }, i) => (
+        {steps.map(({ image, titleText, subtitleText }, i) => (
           <div key={i}>
-            <img src={'onboarding1.png'} alt="onboarding1" height={308} width={308}></img>
+            <img src={image} alt={titleText} height={308} width={308}></img>
             <div className="h-10" />
             <p className="text-lg font-bold text-center">{titleText}</p>
             <div className="h-8" />
@@ -53,11 +45,7 @@ export default function Onboarding() {
         ))}
       </Carousel>
 
-      <div>
-        <button></button>
-        <button></button>
-      </div>
-
+      <div className='h-10'></div>
       <button className="bg-violet-600 py-4 px-12 rounded-full">Launch App</button>
     </div>
   );
