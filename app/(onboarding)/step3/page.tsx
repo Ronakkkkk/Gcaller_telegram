eae95f35a29b5da7dd739d87eaf672da9aeb51a7
Ronak <ronak01.raj@gmail.com>
2024-12-06 02:49:58 +0545

    onboarding

---
+++ app/(onboarding)/step3/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Image from "next/image";
+import styles from "./step3.module.css";
+
+export default function Step1Page() {
+  
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.gradient}></div>
+
+   
+            
+      
+            <div>
+            <h1 className={styles.title}>{"Sucess Full!"}</h1>  
+              <Image
+                src={"/icons/onboarding/onbo5.png"}
+                alt={`Redirect to bot`}
+                width={308}
+                height={308}
+                className={styles.icon}
+              />
+              
+              <p className={styles.description}>{"Contacts Imported Successfully."}</p>
+            </div>
+        
+     
+
+      
+
+      <button className={styles.launchButton}>Continue To App</button>
+    </div>
+  );
+}
