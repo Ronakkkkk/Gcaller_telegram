f1110458a83cba97357b39b1fc1b246046d1a52e
Ronak <ronak01.raj@gmail.com>
2024-12-06 15:03:53 +0545

    onboarding

--- app/(onboarding)/splash/page.tsx
+++ app/(onboarding)/splash/page.tsx
@@ -1,44 +1,42 @@
 'use client';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./splash.module.css";
 import Image from "next/image";
 
-
-
 export default function SplashPage() {
- 
+  const router = useRouter();
 
-  // Simulate loading completion
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowContinue(true);
-  //   }, 5000); // 3-second delay for the loading simulation
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/step1'); // Navigate to Step 1 after 4 seconds
+    }, 4000); // 4-second delay
 
-  //   return () => clearTimeout(timer);
-  // }, []);
-
+    return () => clearTimeout(timer); 
+  }, [router]);
 
   return (
     <div className={styles.container}>
-    <div className={styles.gradient}></div>
-    <div className={styles.logo}>
-      <Image 
-        src="/images/logo.png"
-        alt="Logo"
-        width={144}
-        height={144}
-        priority
-      />
-      <span className={styles.logotext}>CallNetwork</span>
+      <div className={styles.gradient}></div>
+      <div className={styles.logo}>
+        <Image 
+          src="/images/logo.png"
+          alt="Logo"
+          width={144}
+          height={144}
+          priority
+        />
+        <span className={styles.logotext}>CallNetwork</span>
+      </div>
+      <div className={styles.bottomrect}>
+        <Image 
+          src="/images/splashrect.png"
+          alt="Rectangle"
+          width={144}
+          height={144}
+          priority
+        />
+      </div>
     </div>
-    <div className={styles.bottomrect}>
-      <Image 
-        src="/images/splashrect.png"
-        alt="Rectangle"
-        width={144}
-        height={144}
-        priority
-      />
-    </div>
-  </div>
   );
 }
