7695520e15262cddc942aeda6cfef18b8709aa76
Ronak <ronak01.raj@gmail.com>
2024-12-13 13:43:12 +0545

    home screen

---
+++ app/home/components/border.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DashedBorder = () => {
+  return (
+    <div className=" border-dashed w-full mt-[2rem] mb-4 border-t-2 border-gray-500 opacity-45"></div>
+
+  )
+}
+
+export default DashedBorder
