78dcbe9ffc7f9768d8ef62c4ce4c0835f6a869c4
kritan10 <kritant37@gmail.com>
2024-12-21 00:20:46 +0545

    added base url in env file

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
-  images:{
-    unoptimized:true,
+  images: {
+    unoptimized: true,
   },
-  env: {
-    apiUrl: 'http://34.93.164.131/api/v1'
-  }
 };
 
 export default nextConfig;
