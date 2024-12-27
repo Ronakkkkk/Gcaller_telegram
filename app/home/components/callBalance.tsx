1b479b1a4466ccd783c138098a2f7dc6c4e092f8
Ronak <ronak01.raj@gmail.com>
2024-12-27 16:13:05 +0545

    home

--- app/home/components/callBalance.tsx
+++ app/home/components/callBalance.tsx
@@ -1,15 +1,45 @@
 import React from 'react'
+import Card from './card'
+import DashedBorder from './border'
 
 interface CallBalanceProps {
     amount : number
 }
 const CallBalance : React.FC <CallBalanceProps> = ({amount}) => {
   return (
-    <div className=" bg-[#9747FF1A] h-[106px] w-[100%] rounded-2xl border border-gray-500 flex justify-center items-center font-bold text-2xl text-white">
 
-    $CALL BALANCE : {amount}
-  </div>
+    <div className="w-full flex flex-col gap-2">
+      <div className=" bg-[#9747FF1A] h-[106px] w-[100%] rounded-[16px] border border-white/30 flex justify-center items-center font-extrabold text-2xl text-white leading-8">
+
+        $CALL BALANCE : {amount}
+      </div>
+
+      <div className='w-full flex gap-3'>
+
+        <Card
+        imageurl='/icons/home/contacts.png'
+        smallText='Total'
+        biggerText='Contacts'
+        numberOfSpamOrContacts={100}
+        />
+        <Card
+        imageurl='/icons/home/spam.png'
+        smallText='Spam'
+        biggerText='Detected'
+        numberOfSpamOrContacts={300}
+        />
+      </div>
+
+      <DashedBorder/>
+
+    
+
+    </div>
+
+
+  
   )
 }
 
 export default CallBalance
+
---
+++ app/home/components/circularprogresBar.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import React from 'react';
+import { CircularProgress, Typography, Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+// Create custom styled CircularProgress
+const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
+  color: '#9747FF', 
+  '& .MuiCircularProgress-circle': {
+    strokeLinecap: 'round',
+  },
+}));
+
+// Create track CircularProgress
+const TrackCircularProgress = styled(CircularProgress)(({ theme }) => ({
+  color: '#44256b', 
+  position: 'absolute',
+  opacity:30,
+  left: 0,
+  '& .MuiCircularProgress-circle': {
+    strokeLinecap: 'round',
+  },
+}));
+
+const RewardsProgress = ({ value = 75 }) => {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'inline-flex',
+        width: 150,
+        height: 150,
+      }}
+    >
+      {/* Background track progress */}
+      <TrackCircularProgress
+        variant="determinate"
+        value={100}
+        size={150}
+        thickness={4.5}
+        
+      />
+      {/* Main progress */}
+      <StyledCircularProgress
+        variant="determinate"
+        value={value}
+        size={150}
+        thickness={5}
+        
+      />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          variant="body1"
+          component="div"
+          sx={{ color: 'white', fontWeight: 500, fontSize:14 }}
+        >
+          Rewards
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default RewardsProgress;
